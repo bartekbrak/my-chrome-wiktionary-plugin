97861560a7ad21fb687823e570cf2a70c881ba47
Bartek Rychlicki <bartek.rychlicki@gmail.com>
2013-11-26 18:19:16 +0100

    commit

---
+++ dictionary.small.js
@@ -0,0 +1,9 @@
+var dictionary =  {
+    "adolescente": {"definition": "(m) youth (young person), youth (young man), adolescent (a teenager)"},
+    "español": {"definition": "(m) Spanish (of or pertaining to Spain), Spanish (of or pertaining to the people or culture of Spain), Spanish (Romance language of Spain and the Americas), Castilian (Language), Spaniard (somebody from Spain)"},
+    "recibir": {"definition": "get (receive), copy (radio term), receive (get), receive (take possession of), issue (to be produced as an effect or result; to grow or accrue; to arise; to proceed)"},
+    "alguno": {"definition": "someone (some person)"},
+    "vez": {"definition": "(f) time (instance or occurrence)"},
+    "adulto": {"definition": "(m) adult (fully grown human)"},
+    "mensaje": {"definition": "(m) message (a concept conveyed)"},
+  }
